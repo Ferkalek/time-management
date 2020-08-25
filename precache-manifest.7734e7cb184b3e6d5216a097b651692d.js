5335c47da5692d218295009e844f65631e7ca129
Ferkalek <ferkalekanton@gmail.com>
2020-08-25 16:29:51 +0200

    all

---
+++ precache-manifest.7734e7cb184b3e6d5216a097b651692d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53897c1d33b358ba8b2de5c947f8c0ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f4b7ad4e382baf7daa8",
+    "url": "./static/css/2.9694bebf.chunk.css"
+  },
+  {
+    "revision": "3f85473256dcda97c875",
+    "url": "./static/css/main.39f385f8.chunk.css"
+  },
+  {
+    "revision": "6f4b7ad4e382baf7daa8",
+    "url": "./static/js/2.12dd1b0b.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "./static/js/2.12dd1b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f85473256dcda97c875",
+    "url": "./static/js/main.e362acfd.chunk.js"
+  },
+  {
+    "revision": "0dc96efbf301d0b22eae",
+    "url": "./static/js/runtime-main.e10aaafe.js"
+  }
+]);
